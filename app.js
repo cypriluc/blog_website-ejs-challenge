f15b4e8ddcc13ffe80a3dd7c62c1842d32bd0bf4
Lucia <l.cyprianova@gmail.com>
2020-10-23 10:37:57 +0200

    navigation between routes

--- app.js
+++ app.js
@@ -12,21 +12,43 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 app.use(express.static("public"));
 
 
+app.get('/', function(req, res) {
+  res.render('home', {
+    homeStartingContent: homeStartingContent
+  });
+});
 
+app.get('/about', function(req, res) {
+  res.render('about', {
+    aboutContent: aboutContent
+  });
+});
 
+app.get('/contact', function(req, res) {
+  res.render('contact', {
+    contactContent: contactContent
+  });
+});
 
+app.get('/compose', function(req, res) {
+  res.render('compose');
+});
 
-
-
-
-
-
-
-
+app.post('/compose', function(req, res){
+  let postTitle = req.body.postTitle;
+  let postContent = req.body.postContent;
+  let post = {
+    postTitle: postTitle,
+    postContent: postContent
+  }
+  console.log(post);
+})
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
